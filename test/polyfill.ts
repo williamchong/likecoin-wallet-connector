e9a4fb3d7b638abe1af3c83567c90121560e71a6
William Chong <me@williamchong.cloud>
2023-06-27 17:12:04 +0800

    ✅ Add polyfill for test

---
+++ test/polyfill.ts
@@ -0,0 +1,16 @@
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder
+// @ts-ignore
+global.TextDecoder = TextDecoder
+
+window.matchMedia = () => ({
+  matches: true,
+  media: '',
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+})
